ed1c500792596763ce53f293fa5b222877e5821c
swyx <shawnthe1@gmail.com>
2023-01-28 03:10:15 -0500

    tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	let txt = '';
 	/** @type any */
 	let res;
-  let oaikey = '';
+	let oaikey = '';
 
 	async function submit() {
 		res = fetch('/api', {
@@ -17,7 +17,8 @@
 				console.log('api response:', result); // logging this to help users debug
 				return result;
 			} else {
-				throw new Error(res.statusText);
+        console.log('api error:', res);
+				throw res;
 			}
 		});
 	}
@@ -49,37 +50,44 @@
 		<p class="italic">write a prompt...</p>
 	{/if}
 {:catch error}
-	<p class="italic">Something went wrong: Error code {error.code} {error.message}</p>
+	<p class="italic">
+		Something went wrong (either your API key has issues or the API is down). More info in console.
+	</p>
 {/await}
 
 <h2>What is this?</h2>
 
 <p>
-	This is a test of the "Unpwnable" prompt injection protection strategy (<a href="https://simonwillison.net/2022/Sep/12/prompt-injection/">what is prompt injection?</a>). We want to demonstrate
-	that you can take normal product prompts, modify them slightly, and <b>sufficiently protect against prompt injection
-	attacks, WHILE preserving the original functionality</b>.
+	This is a test of the "Unpwnable" prompt injection protection strategy (<a
+		href="https://simonwillison.net/2022/Sep/12/prompt-injection/">what is prompt injection?</a
+	>). We want to demonstrate that you can take normal product prompts, modify them slightly to
+	<b
+		>sufficiently protect against prompt injection attacks, WHILE preserving the original
+		functionality</b
+	>.
 </p>
 <p>
 	First, you can verify that the prompt works as advertised, by submitting topics you would like
-	GPT3 to write about (e.g. "dog", "netlify", "sam altman"). In the API we've used a simple prompt that
-	is meant to be reflective of a realistic product prompt.
+	GPT3 to write about (e.g. "dog", "netlify", "sam altman"). In the API we've used a simple prompt
+	that is meant to be reflective of a realistic product prompt.
 </p>
 <p>
 	Your <b
 		>real mission, should you choose to accept it, is to <a
 			href="https://lspace.swyx.io/p/reverse-prompt-eng">reverse engineer the source prompt</a
 		> to as high fidelity as possible</b
-	>. In other words, try to obtain as much of the "hidden" prefix string that is prepended to your input before it is sent to GPT3.
+	>. In other words, try to obtain as much of the "hidden" prefix string that is prepended to your
+	input before it is sent to GPT3.
 </p>
 <p>
-  There is a rate limit (which is there for my wallet's sake).
-  You can add your OpenAI key here to bypass the rate limit:
-  <input type="text" placeholder="your own OpenAI key" bind:value={oaikey} />
-  {#if oaikey}
-    <span class="whee">will be submitted together with the prompt!</span>
-  {:else}
-  (it's not saved anywhere on my end but you'll have to trust me until I open source this)
-  {/if}
+	There is a rate limit (which is there for my wallet's sake). You can add your OpenAI key here to
+	bypass the rate limit:
+	<input type="text" placeholder="your own OpenAI key" bind:value={oaikey} />
+	{#if oaikey}
+		<span class="whee">will be submitted together with the prompt!</span>
+	{:else}
+		(it's not saved anywhere on my end but you'll have to trust me until I open source this)
+	{/if}
 </p>
 <p><b>Hints/Checks</b>: The source prompt is a simple variation on real product prompts:</p>
 <ul>
@@ -88,27 +96,31 @@
 	<li>ending by concatenating the user input to the source prompt</li>
 	<li>There are NO special characters or formatting used to protect the prompt</li>
 </ul>
-<p>You can leave your guesses and process in the accompanying HN post. We expect that:
-</p>
+<p>You can leave your guesses and process in the accompanying HN post. We expect that:</p>
 <ul>
-  <li>Most will get no more than the first sentence (16 words) of the source prompt.</li> 
-  <li>Some will get the first 3 sentences (34 words).</li>
-  <li>None will get the full 90 words (it MAY be possible to come close, but we think it is IMPOSSIBLE to get the full prompt).</li>
+	<li>Most will get no more than the first sentence (16 words) of the source prompt.</li>
+	<li>Some will get the first 3 sentences (34 words).</li>
+	<li>
+		None will get the full 90 words (it MAY be possible to come close, but we think it is IMPOSSIBLE
+		to get the full prompt).
+	</li>
 </ul>
 <p>
-  We will publish the source prompt and code in a few days; you can then compare your results to the
+	We will publish the source prompt and code in a few days; you can then compare your results to the
 	actual prompt.
 </p>
 <p>
-  It's
-  <a href="https://emn178.github.io/online-tools/sha256.html">SHA-256 hash</a> is
-  <code>f9f91bed2ce346dcb0bc0b6c7dd1bb22c615e3ad0e855fbfe35ac53649c16a97</code> (due to whitespace it would be unrealistic to expect that you will get an exact match, but it'd be a really cool bonus if you do).
+	It's
+	<a href="https://emn178.github.io/online-tools/sha256.html">SHA-256 hash</a> is
+	<code>f9f91bed2ce346dcb0bc0b6c7dd1bb22c615e3ad0e855fbfe35ac53649c16a97</code> (due to whitespace it
+	would be unrealistic to expect that you will get an exact match, but it'd be a really cool bonus if
+	you do).
 </p>
 
 <style>
-  .whee {
-    color: green;
-  }
+	.whee {
+		color: green;
+	}
 	pre {
 		background-color: lightgray;
 		padding: 2rem;
