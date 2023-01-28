a3ce93a8d5b9bdacf7c54698d65737f5e93d9a60
swyx <shawnthe1@gmail.com>
2023-01-28 04:53:20 -0500

    text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,14 +58,15 @@
 <h2>What is this?</h2>
 
 <p>
-	This is a test of the "Unpwnable" prompt injection protection strategy (<a
+	This is a demo of the "Unpwnable" prompt injection protection strategy (<a
 		href="https://simonwillison.net/2022/Sep/12/prompt-injection/">what is prompt injection?</a
-	>). We want to demonstrate that you can take normal product prompts, modify them slightly to
+	>). We want to demonstrate that you can take normal (not contrived, not super defensive) product prompts, and modify them only slightly to
 	<b
 		>sufficiently protect against prompt injection attacks, WHILE preserving the original
 		functionality</b
-	>.
+	>. It's been pwned during the playtest, so "Unpwnable" is a misnomer, but everyone had fun trying so we're just releasing it as is.
 </p>
+<hr />
 <p>
 	First, you can verify that the prompt works as advertised, by submitting topics you would like
 	GPT3 to write about (e.g. "dog", "netlify", "sam altman"). In the API we've used a simple prompt
@@ -80,40 +81,48 @@
 	input before it is sent to GPT3.
 </p>
 <p>
-	There are multiple rate limits (which is there for my wallet's sake). You can add your OpenAI key here to
+	There are multiple rate limits (which is there for my wallet's sake). You can add your OpenAI key here to save me money +
 	bypass all of the rate limits:
 	<input type="text" placeholder="your own OpenAI key" bind:value={oaikey} />
 	{#if oaikey}
 		<span class="whee">will be submitted together with the prompt!</span>
 	{:else}
-		(it's not saved anywhere on my end but you'll have to trust me until I open source this)
+		(it's not saved anywhere on my end but you'll have to trust me until I open source this. You can also set a hard limit in the OpenAI dashboard to protect against abuse, and delete the key when you are done playing.)
 	{/if}
 </p>
-<p><b>Hints/Checks</b>: The source prompt is a simple variation on real product prompts:</p>
-<ul>
-	<li>a 93 word, 517 character string (when pasted in <a href="https://wordcounter.net/">wordcounter</a>)</li>
-	<li>starting with "You are an assistant"</li>
-	<li>ending by concatenating the user input to the source prompt</li>
-	<li>There are NO special characters or formatting (JSON or otherwise), and NO regex or postprocessing used to protect the prompt</li>
-</ul>
+
 <p>You can leave your guesses and process in the accompanying Substack post (dont post guesses on HN or social media in case it spoils it for others!). We expect that:</p>
 <ul>
 	<li>Most will get no more than the first sentence (16 words) of the source prompt.</li>
 	<li>Some will get the first 3 sentences (34 words).</li>
-	<li>Two beta testers have found enough words to win (>80 words verbatim) using 2 different techniques. We've strengthened the protection since, and cannot reproduce them now. Try not to peek anyway! We'll write them up at the end.</li>
+	<li>Two beta testers have found enough words to win (>80 words verbatim) using 2 different techniques. We've strengthened the protection since, and cannot reproduce them now. Try not to peek anyway!</li>
 </ul>
 <p>
-	We will publish the source prompt and code in a few days; you can then compare your results to the
-	actual prompt. Leave an email on <a href="https://lspace.swyx.io/">the substack</a> to get notified when we publish!
+	I will publish the source prompt and code in a few days; you can then compare your results to the
+	actual prompt. <b>Leave an email on <a href="https://lspace.swyx.io/">the substack</a> to get notified</b> when we publish!
 </p>
-<p>
-	It's
-	<a href="https://emn178.github.io/online-tools/sha256.html">SHA-256 hash</a> is
-	<code>cf58ad59e753e80419325ce57901efe40b4e141d819a13b9c0ba2d0c3402de50</code> (due to whitespace it
-	would be unrealistic to expect that you will get an exact match, but it'd be a really cool bonus if
-	you do).
-</p>
+<details>
+	<summary>
+		<b>Hints/Checks</b>
+	</summary>
 
+  The source prompt is a simple variation on real product prompts:
+  <ul>
+    <li>a 93 word, 517 character string (when pasted in <a href="https://wordcounter.net/">wordcounter</a>)</li>
+    <li>starting with "You are an assistant"</li>
+    <li>ending by concatenating the user input to the source prompt</li>
+    <li>There are NO special characters or formatting (JSON or otherwise), and NO regex or postprocessing used to protect the prompt</li>
+  </ul>
+
+  <p>
+    The source prompt's
+    <a href="https://emn178.github.io/online-tools/sha256.html">SHA-256 hash</a> is
+    <code>cf58ad59e753e80419325ce57901efe40b4e141d819a13b9c0ba2d0c3402de50</code> (due to whitespace/nondeterminism/misc reasons, don't bother trying to get an exact match, but it'd be a really cool bonus if
+    you did). Mostly I just want to prove that I'm not lying about the prompt when I eventually release it.
+  </p>
+  
+
+</details>
 <style>
 	.whee {
 		color: green;
